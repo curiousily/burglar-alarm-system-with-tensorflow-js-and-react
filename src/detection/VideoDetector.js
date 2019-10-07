01f9df4ab07e12ce745c44822da3baec47790192
Venelin Valkov <venelin@curiousily.com>
2019-10-07 15:50:25 +0300

    Restyle labels

--- src/detection/VideoDetector.js
+++ src/detection/VideoDetector.js
@@ -1,10 +1,6 @@
 import React, { useEffect, createRef } from "react";
 
-const styles = {
-  position: "absolute",
-  top: 0,
-  left: 0
-};
+const BOUNDING_BOX_LABEL = "Intruder";
 
 const VideoDetector = ({ objectDetector }) => {
   const videoRef = createRef();
@@ -13,7 +9,10 @@ const VideoDetector = ({ objectDetector }) => {
   const detectFromVideoFrame = async video => {
     try {
       const predictions = await objectDetector.detect(video);
-      showDetections(video, predictions);
+
+      const personDetections = predictions.filter(p => p.class === "person");
+
+      showDetections(video, personDetections);
       requestAnimationFrame(() => {
         detectFromVideoFrame(video);
       });
@@ -32,30 +31,22 @@ const VideoDetector = ({ objectDetector }) => {
     ctx.textBaseline = "top";
 
     predictions.forEach(prediction => {
+      ctx.strokeStyle = "red";
+      ctx.lineWidth = 6;
+      ctx.strokeRect(...prediction.bbox);
+
       const x = prediction.bbox[0];
       const y = prediction.bbox[1];
-      const width = prediction.bbox[2];
       const height = prediction.bbox[3];
-      // Draw the bounding box.
-      ctx.strokeStyle = "red";
-      ctx.lineWidth = 4;
-      ctx.strokeRect(x, y, width, height);
-      // Draw the label background.
+
       ctx.fillStyle = "red";
-      const textWidth = ctx.measureText(prediction.class).width;
+      const textWidth = ctx.measureText(BOUNDING_BOX_LABEL).width;
       const textHeight = parseInt(font, 10);
-      // draw top left rectangle
       ctx.fillRect(x, y, textWidth + 10, textHeight + 10);
-      // draw bottom left rectangle
       ctx.fillRect(x, y + height - textHeight, textWidth + 15, textHeight + 10);
 
-      // Draw the text last to ensure it's on top.
-      ctx.fillStyle = "#000000";
-      ctx.fillText(
-        prediction.class === "person" ? "douchebag" : prediction.class,
-        x,
-        y
-      );
+      ctx.fillStyle = "#FFFFFF";
+      ctx.fillText(BOUNDING_BOX_LABEL, x, y);
       ctx.fillText(prediction.score.toFixed(2), x, y + height - textHeight);
     });
   };
@@ -66,16 +57,12 @@ const VideoDetector = ({ objectDetector }) => {
         navigator.mediaDevices.getUserMedia ||
         navigator.mediaDevices.webkitGetUserMedia
       ) {
-        try {
-          const stream = await navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: false
-          });
-          window.stream = stream;
-          videoRef.current.srcObject = stream;
-        } catch (e) {
-          console.log(e.message);
-        }
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: false
+        });
+        window.stream = stream;
+        videoRef.current.srcObject = stream;
       }
     };
     loadCamera();
@@ -89,14 +76,14 @@ const VideoDetector = ({ objectDetector }) => {
     <>
       <video
         onLoadedData={handleLoadedData}
-        style={styles}
         width="1"
         height="1"
         autoPlay
         muted
         ref={videoRef}
       />
-      <canvas style={styles} ref={canvasRef} width="720" height="650" />
+
+      <canvas ref={canvasRef} width="640" height="480" />
     </>
   );
 };
